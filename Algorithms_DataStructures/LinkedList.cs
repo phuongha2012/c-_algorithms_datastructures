4b04719936a982b519d84240f9d85bc05f2c46af
phuongha2012 <hami201289@him-mail.ch>
2020-10-28 10:02:48 +1300

    start solution and implement a linkedlist

---
+++ Algorithms_DataStructures/LinkedList.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Algorithms_DataStructures
+{
+    class LinkedList
+    {
+        int count;
+        LinkedListNode head;
+
+        public LinkedList()
+        {
+            head = null;
+            count = 0;
+        }
+
+        class LinkedListNode
+        {
+            public int data;
+            public LinkedListNode next;
+
+            public LinkedListNode(int x)
+            {
+                data = x;
+                next = null;
+            }
+        }
+
+        public void PrependNode(int data)
+        {
+            LinkedListNode node = new LinkedListNode(data);
+            node.next = head;
+            head = node;
+            count++;
+        }
+
+        public void PrintList()
+        {
+            LinkedListNode current = head;
+            while (current != null)
+            {
+                Console.WriteLine(current.data);
+                current = current.next;
+            }
+        }
+    }
+}
---
+++ Algorithms_DataStructures/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Algorithms_DataStructures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList linkedList = new LinkedList();
+            linkedList.PrependNode(4);
+            linkedList.PrependNode(7);
+            linkedList.PrependNode(2);
+            linkedList.PrependNode(9);
+            linkedList.PrintList();
+        }
+    }
+}
