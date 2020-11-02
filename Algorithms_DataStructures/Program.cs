d69ddc67a3e4d81433646ff3b93b67a0ced94b88
phuongha2012 <hami201289@him-mail.ch>
2020-11-02 15:28:33 +1300

    implement Queue data structure

--- Algorithms_DataStructures/Program.cs
+++ Algorithms_DataStructures/Program.cs
@@ -12,8 +12,6 @@ namespace Algorithms_DataStructures
             linkedList.Prepend(2);
             linkedList.Prepend(9);
             //linkedList.Append(1);
-            //linkedList.PrintReverse();
-            linkedList.PrintReverse();
             linkedList.InsertToPosition(3, 2);
             linkedList.PrintReverse();
         }
---
+++ Algorithms_DataStructures/Queue.cs
@@ -0,0 +1,53 @@
+﻿using System;
+namespace Algorithms_DataStructures
+{
+    class Queue
+    {
+        class Node
+        {
+            public int data;
+            public Node next;
+            public Node(int data)
+            {
+                this.data = data;
+            }
+
+        }
+
+        Node head; // remove things from head
+        Node tail; // add things to tail
+
+        public Queue()
+        {
+            head = null;
+            tail = null;
+        }
+
+        public bool isEmpty()
+        {
+            return head == null;
+        }
+
+        public int peek()
+        {
+            return head.data;
+        }
+
+        public void add(int data)
+        {
+            Node newNode = new Node(data);
+
+            if (tail != null)
+            {
+                tail.next = newNode; 
+            }
+            tail = newNode;
+
+            if (head == null)
+            {
+                head = newNode;
+            }
+        }
+
+    }
+}
